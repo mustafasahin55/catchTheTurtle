bd07d6169904f19fd282dec69da4c6160d6498eb
Mustafa Şahin <mustafasahin8484@gmail.com>
2024-07-12 15:14:38 +0300

    init commit

--- main.py
+++ main.py
@@ -13,17 +13,28 @@ screen.bgcolor('black')
 screen.setup(width=500, height=500)
 t.color('white')
 
-global x
-global y
-x = 0
-y = 0
+global cx
+global cy
+cx = 0
+cy = 0
+global tx, ty
+tx = 0
+ty = 0
+
 score = 0
-def setX(x_n):
-    global x
-    x = x_n
-def setY(y_n):
-    global y
-    y = y_n
+
+
+def setCursorXY(x_n, y_n):
+    global cx, cy
+    cx = x_n
+    cy = y_n
+
+
+def setTurtleXY(x_n, y_n):
+    global tx, ty
+    tx = x_n
+    ty = y_n
+
 
 def goRandomPlace():
     i = random.randint(-225, 225)
@@ -31,21 +42,22 @@ def goRandomPlace():
     t.goto(i, j)
 
 
-def clickedPoint(xx,yy):
-    setX(xx)
-    setY(yy)
+def clickedPoint(xx, yy):
+    setCursorXY(xx, yy)
+
 
 while True:
     goRandomPlace()
     screen.onclick(clickedPoint)
-    t_x, t_y = t.pos()
-    distance = math.sqrt((t_x - x)**2 + (t_y - y)**2)
+    setTurtleXY(t.xcor(), t.ycor())
+    distance = math.sqrt((tx - cx) ** 2 + (ty - cy) ** 2)
+    print("distance:", distance)
+
     if distance < 20:
         print("if")
         score += 1  # score'u 1 artır
-        setX(0)
-        setY(0)
+        setCursorXY(0, 0)
 
-    print('x:',x,'y:',y,"tx:",t_x,"ty:",t_y,'score:',score)
+    print('x:', cx, 'y:', cy, "tx:", tx, "ty:", ty, 'score:', score)
     time.sleep(2)
 turtle.mainloop()
